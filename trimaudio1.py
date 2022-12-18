f1c6d40176fa8b7538c2cb37ab89ceaf0b821df8
Anj1238 <119161294+Anj1238@users.noreply.github.com>
2022-12-18 10:51:00 +0530

    Update trimaudio1.py

--- trimaudio1.py
+++ trimaudio1.py
@@ -1,20 +1,4 @@
-'''from pydub import lakshya.mp3
-sound=AudioSegment.from_lakshya.mp3("lakshya.mp3")
-AudioSegment.converter="ffmpeg.exe"
-AudioSegment.ffmpeg="ffmpeg.exe"
-AudioSegment.ffprobe="ffprobe.exe"
 
-StartMin=0
-StartSec=0
-
-EndMin=0
-EndSec=0.5
-
-StartTime=StartMin*60*1000+StartSec*1000
-EndTime=EndMin*60*1000+EndSec*1000
-
-extract=sound[StartTime:EndTime]
-extract.export("new file.mp3",format="mp3")'''
 
 
 
@@ -25,6 +9,9 @@ from pydub import AudioSegment
 
 #importing file from location by giving its path
 sound = AudioSegment.from_mp3(file="lakshya.mp3")
+AudioSegment.converter="ffmpeg.exe"
+AudioSegment.ffmpeg="ffmpeg.exe"
+AudioSegment.ffprobe="ffprobe.exe"
 
 #Selecting Portion we want to cut
 StrtMin = 0
